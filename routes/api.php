28a0e9912ebbf91238bd3a91997ff4a96b2004f6
Suman Shrestha <summonshr@gmail.com>
2022-03-02 09:31:09 +0530

    WIP

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,2 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('v1')->group(function(){
-    
-    Route::middleware('auth:sanctum')->group(function(){
-        Route::resource('resource','ResourceController');
-    });
-
-    Route::middleware('guest')->group(function(){
-        Route::post('login','AuthController@login');
-        Route::post('sign-up','UserController@create');
-    });
-
-});
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function(){
+    Route::view('/','welcome');
+    Route::resource('resource','ResourceController');
+});
+
+Route::middleware('guest')->group(function(){
+    Route::post('login','AuthController@login');
+    Route::post('sign-up','UserController@create');
 });
