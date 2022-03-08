ee3f6db3479de57b642f98fc029d7e1b976fd70a
sushil <sus.hill.dhakal@gmail.com>
2022-03-08 19:28:06 +0545

    changes on pin page driver form and dashboard build up layout

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth')->group(function(){
-    Route::view('/','welcome');
+   
     Route::resource('resource','ResourceController');
+    
 });
 
 Route::middleware('guest')->group(function(){
     Route::post('login','AuthController@login');
     Route::post('sign-up','UserController@create');
+    
 });
+
+Route::view('/','welcome');
