e2a17573c0ba1aee1ae734080cf29d5d7ede15cb
sushil <sus.hill.dhakal@gmail.com>
2022-04-13 14:18:32 +0545

    changes

--- resources/views/Location/index.blade.php
+++ resources/views/Location/index.blade.php
@@ -21,101 +21,170 @@
                 <div class="page-title-actions">
                     <button type="button" class="btn btn-shadow btn-danger" data-toggle="modal"
                         data-target="#addLocation">Add Location</button>
+                    <button type="button" class="btn btn-shadow btn-danger" data-toggle="modal"
+                        data-target="#addTruck">Add Truck Type</button>
+                    <button type="button" class="btn btn-shadow btn-danger" data-toggle="modal"
+                        data-target="#addSup">Add Supplier</button>
                 </div>
             </div>
         </div>
-
-        <div class="">
-            <div class="main-card mb-3 card">
-                <div class="card-body">
-                    <h5 class="card-title">Locations</h5>
-                    <div class="card-content">
-                        <table id="locationTable" class="table" data-id-field="code" data-sort-name="value2"
-                            data-sort-order="desc" data-show-chart="false" data-pagination="false"
-                            data-show-pagination-switch="false">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Name</th>
-                                    <th>Address</th>
-                                    <th>Truck Type</th>
-                                    <th>Supplier</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>New Aim</td>
-                                    <td>10 jason street, oakleigh south</td>
-                                    <td>Double B</td>
-                                    <td>Agencye</td>
-
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>New Aim</td>
-                                    <td>10 jason street, oakleigh south</td>
-                                    <td>Double B</td>
-                                    <td>Agencye</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>New Aim</td>
-                                    <td>10 jason street, oakleigh south</td>
-                                    <td>Double B</td>
-                                    <td>Agencye</td>
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>New Aim</td>
-                                    <td>10 jason street, oakleigh south</td>
-                                    <td>Double B</td>
-                                    <td>Agencye</td>
-                                </tr>
-                                <tr>
-                                    <td>5</td>
-                                    <td>New Aim</td>
-                                    <td>10 jason street, oakleigh south</td>
-                                    <td>Double B</td>
-                                    <td>Agencye</td>
-                                </tr>
-                                <tr>
-                                    <td>6</td>
-                                    <td>New Aim</td>
-                                    <td>10 jason street, oakleigh south</td>
-                                    <td>Double B</td>
-                                    <td>Agencye</td>
-                                </tr>
-
-                            </tbody>
-                        </table>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="main-card mb-3 card">
+                    <div class="card-body">
+                        <h5 class="card-title">Locations</h5>
+                        <div class="card-content">
+                            <table id="locationTable" class="table" data-id-field="code" data-sort-name="value2"
+                                data-sort-order="desc" data-show-chart="false" data-pagination="false"
+                                data-show-pagination-switch="false">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Name</th>
+                                        <th>Address</th>
+                                        <th>Truck Type</th>
+                                        <th>Supplier</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>New Aim</td>
+                                        <td>10 jason street, oakleigh south</td>
+                                        <td>Double B</td>
+                                        <td>Agencye</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>New Aim</td>
+                                        <td>10 jason street, oakleigh south</td>
+                                        <td>Double B</td>
+                                        <td>Agencye</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>New Aim</td>
+                                        <td>10 jason street, oakleigh south</td>
+                                        <td>Double B</td>
+                                        <td>Agencye</td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>New Aim</td>
+                                        <td>10 jason street, oakleigh south</td>
+                                        <td>Double B</td>
+                                        <td>Agencye</td>
+                                    </tr>
+                                    <tr>
+                                        <td>5</td>
+                                        <td>New Aim</td>
+                                        <td>10 jason street, oakleigh south</td>
+                                        <td>Double B</td>
+                                        <td>Agencye</td>
+                                    </tr>
+                                    <tr>
+                                        <td>6</td>
+                                        <td>New Aim</td>
+                                        <td>10 jason street, oakleigh south</td>
+                                        <td>Double B</td>
+                                        <td>Agencye</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="main-card mb-3 card">
+                    <div class="card-body">
+                        <h5 class="card-title">Truck Type</h5>
+                        <div class="card-content">
+                            <table id="truckTable" class="table" data-id-field="code" data-sort-name="value2"
+                                data-sort-order="desc" data-show-chart="false" data-pagination="false"
+                                data-show-pagination-switch="false">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Truck Type</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Double B</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Double B</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Double B</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="main-card mb-3 card">
+                    <div class="card-body">
+                        <h5 class="card-title">Supplier</h5>
+                        <div class="card-content">
+                            <table id="supTable" class="table" data-id-field="code" data-sort-name="value2"
+                                data-sort-order="desc" data-show-chart="false" data-pagination="false"
+                                data-show-pagination-switch="false">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Supplier</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>XYZ Company</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>XYZ Company</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>XYZ Company</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+            </div>
+        </div>
+        <div class="app-wrapper-footer">
+            <div class="app-footer">
+                <div class="app-footer__inner bg-white">
+                    <div class="app-footer-left">
+                        <ul class="nav">
+                            <li class="nav-item">
+                                &copy; 2022 4th Dimension.
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="app-footer-right">
+                        <ul class="nav">
+                            <li class="nav-item">
+                                Build Version: 0.1
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="app-wrapper-footer">
-        <div class="app-footer">
-            <div class="app-footer__inner bg-white">
-                <div class="app-footer-left">
-                    <ul class="nav">
-                        <li class="nav-item">
-                            &copy; 2022 4th Dimension.
-                        </li>
-                    </ul>
-                </div>
-                <div class="app-footer-right">
-                    <ul class="nav">
-                        <li class="nav-item">
-                            Build Version: 0.1
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
+
 </div>
 
 <div class="modal fade" id="addLocation" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
@@ -156,10 +225,57 @@
                         </select>
                     </div>
 
-                    <!-- <div class="form-group">
-                                    <label>Contact Details</label>
-                                    <input type="text" class="form-control" placeholder="Driver's contact details" required="">
-                                </div> -->
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary btn-lg" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-danger btn-lg">Save changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="addTruck" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-modal="true"
+    data-keyboard="false" data-backdrop="static">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <form class="form w-100">
+                <div class="modal-header">
+                    <h5 class="modal-title font-weight-bold" id="exampleModalLongTitle">Add Truck</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Truck Type Name</label>
+                        <input type="text" class="form-control" placeholder="Truck Type">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary btn-lg" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-danger btn-lg">Save changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="addSup" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-modal="true"
+    data-keyboard="false" data-backdrop="static">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <form class="form w-100">
+                <div class="modal-header">
+                    <h5 class="modal-title font-weight-bold" id="exampleModalLongTitle">Add Supplier</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Supplier Name</label>
+                        <input type="text" class="form-control" placeholder="Supplier">
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary btn-lg" data-dismiss="modal">Close</button>
@@ -196,6 +312,36 @@ $(document).ready(function() {
             ]
         }
     });
+
+
+    $('#truckTable').Tabledit({
+        url: 'http://localhost:8080/location',
+        eventType: 'dblclick',
+        editButton: true,
+        columns: {
+            identifier: [0, 'id'],
+            editable: [
+                [1, 'name'],
+                [2, 'Truck Type']
+
+            ]
+        }
+    });
+
+    $('#supTable').Tabledit({
+        url: 'http://localhost:8080/location',
+        eventType: 'dblclick',
+        editButton: true,
+        columns: {
+            identifier: [0, 'id'],
+            editable: [
+                [1, 'name'],
+                [2, 'Supplier']
+            ]
+        }
+    });
+
+
 })
 </script>
 @endsection
